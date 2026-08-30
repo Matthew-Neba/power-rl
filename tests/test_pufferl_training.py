4492d392c8042a1be44a5ef2dd577bc9da693fac
Matthew Neba <mattneba4343@gmail.com>
2026-08-30 15:27:39 -0600

    Remove CUDA sweep regression tests

--- tests/test_pufferl_training.py
+++ tests/test_pufferl_training.py
@@ -1,54 +1,6 @@
 import pytest
-import torch
 
 from pufferlib import pufferl
-
-
-def test_sweep_cache_release_is_scoped_to_optimizer_device(monkeypatch):
-    calls = []
-
-    class DeviceContext:
-        def __init__(self, device):
-            calls.append(("device", torch.device(device)))
-
-        def __enter__(self):
-            calls.append(("enter",))
-
-        def __exit__(self, *exc):
-            calls.append(("exit",))
-
-    monkeypatch.setattr(torch.cuda, "device", DeviceContext)
-    monkeypatch.setattr(torch.cuda, "synchronize", lambda: calls.append(("synchronize",)))
-    monkeypatch.setattr(torch.cuda, "empty_cache", lambda: calls.append(("empty_cache",)))
-
-    class Optimizer:
-        device = torch.device("cuda:1")
-
-    pufferl._release_sweep_cuda_cache(Optimizer())
-
-    assert calls == [
-        ("device", torch.device("cuda:1")),
-        ("enter",),
-        ("synchronize",),
-        ("empty_cache",),
-        ("exit",),
-    ]
-
-
-@pytest.mark.parametrize("optimizer", [object(), type("CPUOptimizer", (), {"device": "cpu"})()])
-def test_sweep_cache_release_skips_non_cuda_optimizers(monkeypatch, optimizer):
-    monkeypatch.setattr(
-        torch.cuda,
-        "synchronize",
-        lambda: pytest.fail("CUDA synchronization should not run"),
-    )
-    monkeypatch.setattr(
-        torch.cuda,
-        "empty_cache",
-        lambda: pytest.fail("CUDA cache release should not run"),
-    )
-
-    pufferl._release_sweep_cuda_cache(optimizer)
 
 
 def test_train_loads_requested_initial_checkpoint(monkeypatch, tmp_path):
